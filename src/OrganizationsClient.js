7b5131f056403472191773cf383d661825722c58
Mitrich <dima2prog@gmail.com>
2021-09-08 13:57:58 +0400

    Merge conflict

--- src/OrganizationsClient.js
+++ src/OrganizationsClient.js
@@ -5,6 +5,35 @@ export default class OrganizationsClient {
     this.authenticate = authenticate;
   }
  
+  async getOrganizationUsers(orgId) {
+    const url = 'https://diadoc-api.kontur.ru/GetOrganizationUsers?orgId=' + orgId;
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization:
+        'DiadocAuth ddauth_api_client_id=' +
+        process.env.API_CLIENT_ID +
+          ',ddauth_token=' +
+          this.authenticate.getToken()
+      }
+    };
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      if (response.status == 401) {
+        await this.authenticate.auth();
+        return this.getOrganizationUsers(orgId);
+      } else {
+        const text = await response.text();
+        throw new Error(text);
+      }
+    }
+    const text = await response.json();
+    return text;
+  }
+
+
   async getMyOrganizacion() {
     const url = 'https://diadoc-api.kontur.ru/GetMyOrganizations?autoRegister=true';
     const options = {
